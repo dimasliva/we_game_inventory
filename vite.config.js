f97ae30f4fea43cbaf1919783a9397c1f7146fe8
dimasliva <janzerikYT@yandex.ru>
2023-10-05 13:31:45 +0500

    initial

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
+  build: {
+    outDir: 'dist',
+},
   plugins: [
     vue(),
   ],
