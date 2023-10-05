2d1e0927c260010e5eb037cd6c9a036df440d5ff
dimasliva <janzerikYT@yandex.ru>
2023-10-05 15:48:20 +0500

    start page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,67 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1 class="banner"></h1>
+    <div class="logo">
+      <img src="@/assets/logo.png"/>
+    </div>
+    <div class="btn">
+      <button>Играть</button>
+    </div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci obcaecati hic ex placeat veritatis eum blanditiis optio, vel voluptatibus laboriosam consequatur. Ex possimus molestiae sapiente. Libero quibusdam molestias quia dicta.
+    </div>
+    <div class="socials">
+      <img src="@/assets/class.png"/>
+      <img src="@/assets/gos.png"/>
+      <img src="@/assets/vk.svg"/>
+    </div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, consequatur esse. Quisquam, iste alias aspernatur in est porro, eaque nobis suscipit laboriosam repellat dolore perspiciatis sunt vero reiciendis optio praesentium.
+    </div>
+  </div>
 </template>
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px;
+  }
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 210px;
+  }
+  .logo img {
+    height: 100%;
+  }
+  .btn {
+    margin: 20px 0px;
+    height: 60px;
+    width: 50%;
+  }
+  .btn button {
+    width: 100%;
+    height: 100%;
+    border: 0;
+    background-color: green;
+    color: white;
+    font-size: 20px;
+    border-radius: 4px;
+  }
+  .socials {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 75px;
+    width: 100%;
+    margin: 10px 0px;
+  }
+  .socials img {
+    height: 100%;
+  } 
+</style>
